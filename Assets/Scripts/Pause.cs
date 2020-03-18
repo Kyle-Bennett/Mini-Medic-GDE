a4a23d23c54d4f98e311bc0ad26a2387d9b3f992
Jake Martin <Jake.Martin3@live.uwe.ac.uk>
2020-03-18 20:33:07 +0000

    scene changes/ management/ pause menu

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Pause : MonoBehaviour
+{
+
+	public static bool GameIsPaused = false;
+
+	public GameObject pauseMenuUI;
+
+	
+	void Start()
+    {
+		pauseMenuUI.SetActive(false);
+	}
+
+    void Update()
+    {
+		if (Input.GetKeyDown(KeyCode.Escape))
+		{
+
+			if (GameIsPaused)
+			{
+				Resume();
+			}
+			else
+			{
+				PauseGame();
+			}
+		}
+	}
+
+	public void Resume()
+	{
+		pauseMenuUI.SetActive(false);
+		Time.timeScale = 1f;
+		GameIsPaused = false;
+	}
+
+	public void PauseGame()
+	{
+		pauseMenuUI.SetActive(true);
+		Time.timeScale = 0f;
+		GameIsPaused = true;
+	}
+
+	public void MainMenu()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+}
---
+++ Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+
+
+	public void Quit()
+	{
+		SceneManager.LoadScene("MainMenu");
+	}
+
+	public void levelSelect()
+	{
+		SceneManager.LoadScene("LevelSelect");
+	}
+
+	public void loadGame()
+	{
+		SceneManager.LoadScene("Game");
+	}
+
+	
+}
